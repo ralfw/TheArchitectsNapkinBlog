9d7cf6ec06d0afcb258811c17a00a6c97fd94185
Ralf Westphal <info@ralfw.de>
2015-05-04 18:24:34 +0200

    Started spike for communication between head and body actors

--- IODAsample_alarmclock/spike.akka/Form1.cs
+++ IODAsample_alarmclock/spike.akka/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
 
 namespace spike.akka
 {
@@ -16,5 +17,20 @@ namespace spike.akka
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.OnDataEntered(this.textBox1.Text);
+        }
+
+
+        public void Display_message(string msg)
+        {
+            this.label1.Text = msg;
+            this.listBox1.Items.Insert(0, msg);
+        }
+
+
+        public event Action<string> OnDataEntered;
     }
 }
--- IODAsample_alarmclock/spike.akka/Program.cs
+++ IODAsample_alarmclock/spike.akka/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using Akka.Actor;
 
 namespace spike.akka
 {
@@ -16,7 +14,48 @@ namespace spike.akka
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+
+            var sys = ActorSystem.Create("MyActorSystem");
+
+            var bodyActorProps = Props.Create<BodyActor>();
+            var bodyActor = sys.ActorOf(bodyActorProps, "BodyActor");
+
+            var dlg = new Form1();
+            var headActorProps = Props.Create<HeadActor>(dlg, bodyActor)
+                                      .WithDispatcher("akka.actor.synchronized-dispatcher");
+            var headActor = sys.ActorOf(headActorProps, "HeadActor");
+
+            headActor.Tell("hello");
+            Application.Run(dlg);
+        }
+    }
+
+
+    internal class HeadActor : ReceiveActor
+    {
+        public HeadActor(Form1 dlg, IActorRef onDataEntered)
+        {
+            dlg.OnDataEntered += msg => {
+                Console.WriteLine("head {0}", System.Threading.Thread.CurrentThread.GetHashCode());
+                onDataEntered.Tell(msg);
+            };
+
+            Receive<string>(msg => {
+                dlg.Display_message(msg);
+            });
+        }
+    }
+
+
+    internal class BodyActor : ReceiveActor
+    {
+        public BodyActor()
+        {
+            Receive<string>(msg =>
+            {
+                Console.WriteLine("body {0}", System.Threading.Thread.CurrentThread.GetHashCode());
+                this.Sender.Tell(msg.ToUpper());
+            });
         }
     }
 }
