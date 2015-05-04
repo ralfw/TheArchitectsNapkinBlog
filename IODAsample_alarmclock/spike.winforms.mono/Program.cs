377b8810d1ab54ff6ab70bcf46ac13311c935beb
Ralf Westphal <info@ralfw.de>
2015-05-04 16:17:08 +0200

    Started IODA sample 'alarm clock'

---
+++ IODAsample_alarmclock/spike.winforms.mono/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+public class HelloWorld : Form
+{
+    static public void Main()
+    {
+        Application.Run(new HelloWorld());
+    }
+
+    public HelloWorld()
+    {
+        Button b = new Button();
+        b.Text = "Click Me!";
+        b.Click += new EventHandler(Button_Click);
+        Controls.Add(b);
+    }
+
+    private void Button_Click(object sender, EventArgs e)
+    {
+        MessageBox.Show("Button Clicked!");
+    }
+}
