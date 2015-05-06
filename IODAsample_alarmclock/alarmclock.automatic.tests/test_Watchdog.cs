1501b2319b617c0b1c2b740289016e9288a7d3e3
Ralf Westphal <info@ralfw.de>
2015-05-06 16:48:48 +0200

    Started dev on Watchdog

---
+++ IODAsample_alarmclock/alarmclock.automatic.tests/test_Watchdog.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using alarmclock.body;
+
+namespace alarmclock.automatic.tests
+{
+	[TestFixture ()]
+	public class test_Watchdog
+	{
+		[Test ()]
+		public void Nothing_discovered ()
+		{
+			var sut = new Watchdog ();
+
+			TimeSpan result = TimeSpan.MinValue;
+			sut.OnRemainingTime += _ => result = _;
+
+			sut.Start_watching_for (new DateTime (10, 0, 0));
+			sut.Check (new DateTime (9, 55, 35));
+
+			Assert.AreEqual (new TimeSpan(0,4,25), result);
+		}
+	}
+}
---
+++ IODAsample_alarmclock/alarmclock.body/Watchdog.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace alarmclock.body
+{
+	public class Watchdog {
+		private bool watching;
+		private DateTime wakeuptime;
+
+		public void Start_watching_for(DateTime wakeuptime) {
+			this.wakeuptime = wakeuptime;
+			this.watching = true;
+		}
+
+		public void Stop_watching() {
+			this.watching = false;
+		}
+
+		public void Check(DateTime currentTime) {
+			if (this.watching) {
+				var remainingTime = this.wakeuptime.Subtract (currentTime);
+				this.OnRemainingTime (remainingTime);
+				if (remainingTime.TotalSeconds <= 0) {
+					this.watching = false;
+					this.OnWakeuptimeDiscovered ();
+				}
+			}
+		}
+
+		public event Action<TimeSpan> OnRemainingTime;
+		public event Action OnWakeuptimeDiscovered;
+	}
+}
