c041e410abae0a18049c38e1ef4be9a683873fa8
Ralf Westphal <info@ralfw.de>
2015-05-07 20:17:41 +0200

    spike solution for flow with unknown number of downstream functional units

--- IODAsample_alarmclock/alarmclock.automatic.tests/test_Watchdog.cs
+++ IODAsample_alarmclock/alarmclock.automatic.tests/test_Watchdog.cs
@@ -15,10 +15,48 @@ namespace alarmclock.automatic.tests
 			TimeSpan result = TimeSpan.MinValue;
 			sut.OnRemainingTime += _ => result = _;
 
-			sut.Start_watching_for (new DateTime (10, 0, 0));
-			sut.Check (new DateTime (9, 55, 35));
+			sut.Start_watching_for (new DateTime (2015, 1, 1, 10, 0, 0));
+			sut.Check (new DateTime (2015, 1, 1, 9, 55, 35));
 
 			Assert.AreEqual (new TimeSpan(0,4,25), result);
+		}
+
+
+		[Test ()]
+		public void Wakeuptime_discovered ()
+		{
+			var sut = new Watchdog ();
+
+			var result = TimeSpan.MinValue;
+			sut.OnRemainingTime += _ => result = _;
+
+			var resultDiscovery = false;
+			sut.OnWakeuptimeDiscovered += () => resultDiscovery = true;
+
+			sut.Start_watching_for (new DateTime (2015, 1, 1, 10, 0, 0));
+			sut.Check (new DateTime (2015, 1, 1, 10, 0, 0));
+
+			Assert.AreEqual (new TimeSpan(0,0,0), result);
+			Assert.IsTrue (resultDiscovery);
+		}
+
+
+		[Test ()]
+		public void Wakeuptime_discovered_even_if_already_past ()
+		{
+			var sut = new Watchdog ();
+
+			var result = TimeSpan.MinValue;
+			sut.OnRemainingTime += _ => result = _;
+
+			var resultDiscovery = false;
+			sut.OnWakeuptimeDiscovered += () => resultDiscovery = true;
+
+			sut.Start_watching_for (new DateTime (2015, 1, 1, 10, 0, 0));
+			sut.Check (new DateTime (2015, 1, 1, 10, 1, 10));
+
+			Assert.AreEqual (new TimeSpan(0,0,0), result);
+			Assert.IsTrue (resultDiscovery);
 		}
 	}
 }
